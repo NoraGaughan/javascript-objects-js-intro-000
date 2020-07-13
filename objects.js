3a234501ac7db13e40e806ce9a7807fd8440c979
Nora Gaughan <noragaughan@gmail.com>
2020-07-13 16:23:01 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,2 +1,2 @@
 var playlist = new Object({keys: '0'})
-function updatePlaylist(playlist, artistName, songTitle) {playlist
+function updatePlaylist(playlist, artistName, songTitle) {playlist[artistName]
