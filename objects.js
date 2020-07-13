6a6b20c8f1699f46a2a04e24bfbe2b37fcbb61ec
Nora Gaughan <noragaughan@gmail.com>
2020-07-13 16:21:42 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,13 +1,1 @@
 var playlist = { artistName: 'songTitle'};
-
-function updatePlaylist(thePlaylist, artistName, songTitle) {
-  return Object.assign({}, thePlaylist, { [artistName]: songTitle});} 
-
-updatePlaylist(playlist, "Alison", "Slowdive");
-
-function removeFromPlaylist(thePlaylist, artistName) {
-  delete thePlaylist[artistName];
-  return thePlaylist;
-}
-
-removeFromPlaylist(playlist, "artist2");
